c33f060edbc8bc87262a99726db1d894b611b46e
Maciej Cieslar <maciejcieslar@divante.pl>
2023-11-12 16:35:45 +0100

    feat: add setup & alpha working version

---
+++ src/manifest.ts
@@ -0,0 +1,34 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageData from '../package.json'
+
+export default defineManifest({
+  name: packageData.name,
+  description: packageData.description,
+  version: packageData.version,
+  manifest_version: 3,
+  icons: {
+    16: 'img/logo-16.png',
+    32: 'img/logo-34.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
+  },
+  action: {
+    default_popup: 'popup.html',
+    default_icon: 'img/logo-48.png',
+  },
+  content_scripts: [
+    {
+      matches: ['*://*.twitch.tv/*'],
+      js: ['src/contentScript/index.ts'],
+      run_at: 'document_end',
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      matches: [],
+    },
+  ],
+  host_permissions: ['*://*.twitch.tv/*'],
+  permissions: ['scripting', 'storage', 'activeTab'],
+})
